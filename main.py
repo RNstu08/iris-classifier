80d06a09491645f4b18d002dcdb10a0a049f02d5
RNStu08 <exampledemo8@gmail.com>
2025-04-28 18:20:00 +0200

    Iris classification with RandomForest

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
 
 # Load the Iris dataset
 data = load_iris()
@@ -34,5 +37,22 @@ clf.fit(X_train, y_train)
 # Print the best hyperparameters and score
 print("Best Parameters:", clf.best_params_)
 print("Best Score:", clf.best_score_)
+
+
+
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
+
+clf = RandomForestClassifier()
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=iris.target_names)
+disp.plot(cmap="Blues")
+plt.title("Confusion Matrix - Random Forest on Iris")
+#plt.show()
+plt.savefig("pca_plot.png")
 
 
